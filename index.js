bc84f010d432ae75aa1148469296da4d8a52817c
charles yarney <charlesyarney@gmail.com>
2021-11-10 15:40:29 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+function stripUpperCase(string){
+  let stripped = ""
+  for (let i = 0; i  < string.length; i++){
+      if (string[i] === string[i].toLowerCase())
+      stripped += string[i];
+  }
+  return stripped
+}
+
+console.log(stripUpperCase('Hello!'))
